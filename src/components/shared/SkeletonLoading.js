b307c57da5edf8495bec0b99253cd0a50d10e665
RakibHasanDev <rakibkst16@gmail.com>
2025-03-19 02:34:01 +0600

    added dashboard  and fix loading

--- src/components/shared/SkeletonLoading.js
+++ src/components/shared/SkeletonLoading.js
@@ -3,7 +3,7 @@ import React from "react";
 const SkeletonLoading = () => {
   return (
     <div>
-      <div className="py-4 rounded shadow-md w-full animate-pulse bg-[#00a6b25d] dark:bg-gray-900 min-h-[82vh]">
+      <div className="py-4 rounded shadow-md w-full animate-pulse bg-[#00a6b25d] dark:bg-gray-900 min-h-[90vh]">
         <div className="flex p-4 space-x-4 sm:px-8">
           <div className="flex-shrink-0 w-16 h-16 rounded-full dark:bg-gray-700"></div>
           <div className="flex-1 py-2 space-y-4">
